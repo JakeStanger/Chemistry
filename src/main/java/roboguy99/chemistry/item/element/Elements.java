4638ef509d84498e5f394c9d02259725ede6df91
Roboguy99 <Roboguy99@gmail.com>
2016-03-27 23:38:20 +0100

    Removed compounds. Implemented temporary image generation code.

--- src/main/java/roboguy99/chemistry/item/element/Elements.java
+++ src/main/java/roboguy99/chemistry/item/element/Elements.java
@@ -1,8 +1,16 @@
 package roboguy99.chemistry.item.element;
 
-import java.util.LinkedHashMap;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-import roboguy99.chemistry.api.EnumColour;
+import javax.imageio.ImageIO;
+
 import roboguy99.chemistry.api.EnumElement;
 import roboguy99.chemistry.item.compound.Compound;
 import roboguy99.chemistry.item.element.elements.Berylium;
@@ -33,66 +41,7 @@ public class Elements
 		elements[7] = new Oxygen();
 		elements[91] = new Uranium();
 		
-		LinkedHashMap waterCompound = new LinkedHashMap(); //TODO Move all of these to their own class
-		waterCompound.put(getElement(EnumElement.HYDROGEN), 2);
-		waterCompound.put(getElement(EnumElement.OXYGEN), 1);
-		Compound water = new Compound(waterCompound);
-		
-		LinkedHashMap glucoseCompound = new LinkedHashMap();
-		glucoseCompound.put(getElement(EnumElement.CARBON), 6);
-		glucoseCompound.put(getElement(EnumElement.HYDROGEN), 12);
-		glucoseCompound.put(getElement(EnumElement.OXYGEN), 6);
-		Compound glucose = new Compound(glucoseCompound);
-		
-		/*LinkedHashMap ethanolCompoundMain = new LinkedHashMap();
-		ethanolCompoundMain.put(Elements.carbon, 2);
-		ethanolCompoundMain.put(Elements.hydrogen, 5);
-		Compound ethanolMain = new Compound(ethanolCompoundMain);
-		
-		LinkedHashMap ethanolCompoundFunctional = new LinkedHashMap();
-		ethanolCompoundFunctional.put(Elements.oxygen, 1);
-		ethanolCompoundFunctional.put(Elements.hydrogen, 1);
-		Compound ethanolFunctional = new Compound(ethanolCompoundFunctional);
-		
-		Compound[] compoundArray = {ethanolMain, ethanolFunctional};
-		ethanol = new Compound(compoundArray);
-		
-		/*String text = "H"; TODO Automate this for every element
-
-        /*
-           Because font metrics is based on a graphics context, we need to create
-           a small, temporary image so we can ascertain the width and height
-           of the final image
-         
-        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = img.createGraphics();
-        Font font = new Font("Arial", Font.PLAIN, 48);
-        g2d.setFont(font);
-        FontMetrics fm = g2d.getFontMetrics();
-        //int width = fm.stringWidth(text);
-        //int height = fm.getHeight();
-        g2d.dispose();
-
-        img = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
-        g2d = img.createGraphics();
-        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
-        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
-        g2d.setFont(font);
-        fm = g2d.getFontMetrics();
-        g2d.setColor(Color.BLACK);
-        g2d.drawString(text, 0, fm.getAscent());
-        g2d.dispose();
-        try {
-            ImageIO.write(img, "png", new File("H.png"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }*/
+		this.generateTexturesForElements();
     }
 	
 	/**
@@ -113,5 +62,49 @@ public class Elements
 	public static Element getElement(EnumElement enumElement)
 	{
 		return getElement(enumElement.atomicNumber);
+	}
+	
+	private void generateTexturesForElements() //TODO Remove this once all elements are created
+	{
+		for(Element element : this.elements)
+		{
+			if(element != null && !new File("generatedtextures" + element.getName() + "png").isFile()) 
+			{
+				String text = element.getSymbol();
+			
+		        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+		        Graphics2D g2d = img.createGraphics();
+		        Font font = new Font("Arial", Font.PLAIN, 48);
+		        g2d.setFont(font);
+		        FontMetrics fm = g2d.getFontMetrics();
+		        //int width = fm.stringWidth(text);
+		        //int height = fm.getHeight();
+		        g2d.dispose();
+		
+		        img = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+		        g2d = img.createGraphics();
+		        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+		        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+		        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+		        g2d.setFont(font);
+		        fm = g2d.getFontMetrics();
+		        g2d.setColor(Color.BLACK);
+		        g2d.drawString(text, 0, fm.getAscent());
+		        g2d.dispose();
+		        try 
+		        {
+		            ImageIO.write(img, "png", new File("generatedtextures/" + element.getName() + ".png"));
+		        } 
+		        catch (IOException ex) 
+		        {
+		            ex.printStackTrace();
+		        }
+			}
+		}
 	}
 }
