0c06aaaefc241caf4dc4685c1094b21824022b11
Roboguy99 <Roboguy99@gmail.com>
2016-03-28 20:26:22 +0100

    Temporarily stopped from being tileentity for easy debugging

--- src/main/java/roboguy99/chemistry/block/BlockCompoundAnalyser.java
+++ src/main/java/roboguy99/chemistry/block/BlockCompoundAnalyser.java
@@ -1,15 +1,23 @@
 package roboguy99.chemistry.block;
 
-import net.minecraft.block.BlockContainer;
+import java.util.Random;
+
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import roboguy99.chemistry.Chemistry;
-import roboguy99.chemistry.tileentity.TileCompoundAnalyser;
+import roboguy99.chemistry.api.EnumElement;
+import roboguy99.chemistry.item.compound.CompoundCreator;
+import roboguy99.chemistry.item.element.Elements;
 
-public class BlockCompoundAnalyser extends BlockContainer
+public class BlockCompoundAnalyser extends Block/*extends BlockContainer*/
 {
 	public BlockCompoundAnalyser() 
 	{
@@ -18,7 +26,7 @@ public class BlockCompoundAnalyser extends BlockContainer
 		GameRegistry.registerBlock(this, "blockCompoundMaker");
 	}
 
-	public int getRenderType()
+	/*public int getRenderType()
 	{
 		return -1;
 	}
@@ -36,11 +44,24 @@ public class BlockCompoundAnalyser extends BlockContainer
 	public TileEntity createNewTileEntity(World world, int var2) 
 	{
 		return new TileCompoundAnalyser();
-	}
+	}*/
 	
-	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ)
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side, float hitX, float hitY, float hitZ)
 	{
-		//Open GUI
+		if(!world.isRemote)
+		{
+			CompoundCreator compoundHandler = new CompoundCreator();
+				
+			compoundHandler.putElement(Elements.getElement(EnumElement.CARBON), 6);
+			compoundHandler.putElement(Elements.getElement(EnumElement.HYDROGEN), 12);
+			compoundHandler.putElement(Elements.getElement(EnumElement.OXYGEN), 6);
+				
+			ItemStack stack = compoundHandler.createCompound();
+			System.out.println(stack.getTagCompound());
+			compoundHandler.clearElements();
+			
+			world.spawnEntityInWorld(new EntityItem(world, pos.getX(), pos.getY(), pos.getZ(), stack));
+		}
 		return true;
 	}
 }
