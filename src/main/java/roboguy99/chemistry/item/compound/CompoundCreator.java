a242a0aff5a59df62e2231d1531851faf2aeef03
Roboguy99 <Roboguy99@gmail.com>
2016-03-29 23:03:55 +0100

    Tidied and commented code, added some more methods

--- src/main/java/roboguy99/chemistry/item/compound/CompoundCreator.java
+++ src/main/java/roboguy99/chemistry/item/compound/CompoundCreator.java
@@ -6,7 +6,9 @@ import java.util.List;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import roboguy99.chemistry.Chemistry;
+import roboguy99.chemistry.api.EnumElement;
 import roboguy99.chemistry.item.element.Element;
+import roboguy99.chemistry.item.element.Elements;
 import roboguy99.chemistry.network.packet.CompoundCreate;
 
 /**
@@ -53,21 +55,61 @@ public class CompoundCreator
 		return stack;
 	}
 	
+	/**
+	 * Add the given element to the buffer
+	 * @param element The element to add
+	 */
 	public void putElement(Element element)
 	{
 		this.elements.add(element);
 	}
 	
+	/**
+	 * Add the given element to the buffer
+	 * @param element The element to add
+	 * @param quantity The number of times to add the element
+	 */
 	public void putElement(Element element, int quantity)
 	{
-		for(int i = 0; i < quantity; i++) this.elements.add(element);
+		for(int i = 0; i < quantity; i++) this.putElement(element);
 	}
 	
+	/**
+	 * Add the given list of elements to the buffer
+	 * @param elements The list of elements to add
+	 */
+	public void putElements(List<Element> elements)
+	{
+		for(Element element : elements) this.putElement(element);
+	}
+	
+	/**
+	 * Add the given list of elements to the buffer
+	 * @param elements The list of elements to add
+	 * @param quantity The number of times to add the element
+	 */
+	public void putElements(List<Element> elements, int quantity)
+	{
+		for(int i = 0; i < quantity; i++)
+		{
+			this.putElements(elements);
+			this.elements.add(Elements.getElement(EnumElement.MOLECULE_MARKER)); //Add a molecule marker after each so it's factorised
+		}
+		this.elements.remove(this.elements.size()-1); //Remove trailing molecule marker
+	}
+	
+	/**
+	 * Return a list of the elements in the buffer
+	 * @return The elements currently in the buffer
+	 */
 	public List<Element> getElements()
 	{
 		return this.elements;
 	}
 	
+	/**
+	 * Clear all elements from the buffer
+	 */
 	public void clearElements()
 	{
 		this.elements.clear();
