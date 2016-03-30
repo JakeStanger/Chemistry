f33b4b160d6a23b97c9fb67546d0b4a9c0fcb1e6
Roboguy99 <Roboguy99@gmail.com>
2016-03-30 01:28:26 +0100

    Created new element

---
+++ src/main/java/roboguy99/chemistry/item/element/elements/Aluminium.java
@@ -0,0 +1,49 @@
+package roboguy99.chemistry.item.element.elements;
+
+import roboguy99.chemistry.item.element.Element;
+
+public class Aluminium extends Element
+{
+	@Override
+	public String giveName()
+	{
+		return "aluminium";
+	}
+
+	@Override
+	public String giveSymbol()
+	{
+		return "Al";
+	}
+
+	@Override
+	public int giveAtomicNumber()
+	{
+		return 13;
+	}
+
+	@Override
+	public int giveAtomicMass()
+	{
+		return 27;
+	}
+
+	@Override
+	public int giveMeltingPoint()
+	{
+		return 660;
+	}
+
+	@Override
+	public int giveBoilingPoint()
+	{
+		return 2519;
+	}
+
+	@Override
+	public int[] giveElectronConfiguration()
+	{
+		return new int[]{2,8,3};
+	}
+	
+}
