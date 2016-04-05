6aad31b247715ffce5ffa83848401487d85b4bb1
Roboguy99 <Roboguy99@gmail.com>
2016-04-05 16:30:40 +0100

    Vastly improved name system. New methods and plenty of comments.

--- src/main/java/roboguy99/chemistry/api/CompoundNames.java
+++ src/main/java/roboguy99/chemistry/api/CompoundNames.java
@@ -9,18 +9,67 @@ public class CompoundNames
 	 * Formulas should be structural, and without subscript
 	 * (C2H5OH)
 	 */
-	public static HashMap<String, String> names;
+	private static HashMap<String, String> names;
 	
 	public CompoundNames()
 	{
 		names = new HashMap<String, String>();
 		
-		names.put("H2O", "compound.water");
-		names.put("C6H2(NO2)3CH3", "compound.tnt");
+		//Example names
+		this.putName("H2O", "water");
+		this.putName("C6H2(NO2)3CH3", "tnt");
 	}
 	
+	/**
+	 * Puts the given formula and name into the map.
+	 * Formulas should be as they would be displayed on screen, but without superscript.
+	 * For example, water would be <i>H2O</i> or TNT would be <i>C6H2(NO2)3CH3</i>.
+	 * @param formula The formula to give a name for
+	 * @param unlocalisedName The unlocalised name for the formula
+	 */
 	public static void putName(String formula, String unlocalisedName)
 	{
 		names.put(formula, "compound." + unlocalisedName);
+	}
+	
+	/**
+	 * Gets the entire names map.
+	 * @return The map of all formulas and their respective names.
+	 */
+	public static HashMap<String, String> getNames()
+	{
+		return names;
+	}
+	
+	/**
+	 * Gets the name for a given formula.
+	 * @param formula The formula to get the name for.
+	 * @return The name of the given formula.
+	 */
+	public static String getName(String formula)
+	{
+		return names.get(formula);
+	}
+	
+	/**
+	 * Checks if the given name is already in the map.
+	 * @param name The unlocalised compound name to check
+	 * @return True if the name is in the map.
+	 */
+	public static boolean isFormulaInMap(String name)
+	{
+		return names.containsKey(name);
+	}
+	
+	/**
+	 * Checks if the given formula is already in the map.
+	 * Formulas should be as they would be displayed on screen, but without superscript.
+	 * For example, water would be <i>H2O</i> or TNT would be <i>C6H2(NO2)3CH3</i>.
+	 * @param formula The formula name to check
+	 * @return True if the formula is in the map.
+	 */
+	public static boolean isNameInMap(String formula)
+	{
+		return names.containsValue(formula);
 	}
 }
