a2c11beef2746aa68ff6f138ccefff5ae759ff24
Roboguy99 <Roboguy99@gmail.com>
2016-04-05 16:06:40 +0100

    Overrode information with nothing

--- src/main/java/roboguy99/chemistry/item/element/elements/special/ElementSpecial.java
+++ src/main/java/roboguy99/chemistry/item/element/elements/special/ElementSpecial.java
@@ -1,11 +1,15 @@
 package roboguy99.chemistry.item.element.elements.special;
 
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import roboguy99.chemistry.item.element.Element;
 
 /**
  * An element used for mod utility purposes.
- * If you require an element that you do not wish to be easily accessible, then extend this.
- * This class does not add the element to any creative tab.
+ * If you require an element that is not part of your periodic table, 
+ * but you need for some internal mechanism, extend this.
  * For example, see the MoleculeMarker element.
  * @author Roboguy99
  *
@@ -15,4 +19,8 @@ public abstract class ElementSpecial extends Element
 	/*@Override
 	protected void addToCreativeTab()
 	{}*/
+	
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced)
+	{}
 }
