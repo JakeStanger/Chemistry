4cfca502acfec92365d31c900eb614655a7a4006
robog <robog@JakeDesktop.Home>
2016-08-06 23:33:24 +0100

    Changed to use new BlockTile

--- src/main/java/roboguy99/chemistry/block/BlockCompoundCreator.java
+++ src/main/java/roboguy99/chemistry/block/BlockCompoundCreator.java
@@ -20,62 +20,17 @@ import roboguy99.chemistry.Chemistry;
 import roboguy99.chemistry.handler.GuiHandler;
 import roboguy99.chemistry.tile.TileCompoundCreator;
 
-public class BlockCompoundCreator extends BlockContainer
+public class BlockCompoundCreator extends BlockTile
 {
 	public BlockCompoundCreator() 
 	{
-		super(Material.IRON);
-		this.setCreativeTab(Chemistry.tabMachines);
-		this.setUnlocalizedName("blockCompoundCreator");
-		this.setRegistryName("blockCompoundCreator");
-		
-		GameRegistry.register(this);
-		
-		ItemBlock itemBlock = new ItemBlock(this);
-		GameRegistry.register(itemBlock);
-	}
-	
-	
-	@Override
-	public void breakBlock(World world, BlockPos pos, IBlockState blockstate) 
-	{
-	    TileCompoundCreator te = (TileCompoundCreator) world.getTileEntity(pos);
-	    InventoryHelper.dropInventoryItems(world, pos, te);
-	    super.breakBlock(world, pos, blockstate);
-	}
-
-
-	@Override
-	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) 
-	{
-	    if (stack.hasDisplayName()) 
-	    {
-	        ((TileCompoundCreator) worldIn.getTileEntity(pos)).setCustomName(stack.getDisplayName());
-	    }
-	}
-
-	/*@Override
-	public int getRenderType()
-	{
-		return 3;
-	}
-	
-	@Override
-	public boolean isOpaqueCube()
-	{
-		return false;
-	}*/
-	
-	@Override
-	public TileEntity createNewTileEntity(World world, int var2) 
-	{
-		return new TileCompoundCreator();
+		super(Material.IRON, "blockCompoundCreator", Chemistry.tabMachines, TileCompoundCreator.class);
 	}
 	
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
 	{
-		if(!world.isRemote)player.openGui(Chemistry.INSTANCE, GuiHandler.GUI_BLOCK_COMPOUND_CREATOR, world, pos.getX(), pos.getY(), pos.getZ());
+		if(!world.isRemote) player.openGui(Chemistry.INSTANCE, GuiHandler.GUI_BLOCK_COMPOUND_CREATOR, world, pos.getX(), pos.getY(), pos.getZ());
 		return true;
 	}
 	
