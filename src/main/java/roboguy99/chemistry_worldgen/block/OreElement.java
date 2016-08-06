09de4fc4537ef6aa3184630a9cc3f566668c73f4
robog <robog@JakeDesktop.Home>
2016-08-06 23:34:59 +0100

    Tweaks and improvements

--- src/main/java/roboguy99/chemistry_worldgen/block/OreElement.java
+++ src/main/java/roboguy99/chemistry_worldgen/block/OreElement.java
@@ -1,9 +1,10 @@
 package roboguy99.chemistry_worldgen.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import roboguy99.chemistry.Chemistry;
 import roboguy99.chemistry.item.element.Element;
 import roboguy99.chemistry_worldgen.ChemistryWorldGen;
 
@@ -13,15 +14,26 @@ public class OreElement extends Block
 	
 	public OreElement(Element element)
 	{	
-		super(Blocks.STONE.getMaterial(Blocks.STONE.getDefaultState())); //TODO Find replacement for this
-		this.name = element.getName().replaceAll("element_", "").toLowerCase();
+		super(Material.ROCK); //TODO Find replacement for this
+		this.name = "ore_" + element.getName().replaceAll("element_", "").toLowerCase();
 		
 		this.setCreativeTab(ChemistryWorldGen.tabOre);
-		this.setUnlocalizedName("ore_" + name);
-		this.setRegistryName("ore_" + name);
+		this.setUnlocalizedName(name);
+		this.setRegistryName(name);
+		this.setHardness(3F);
+		this.setHarvestLevel("pickaxe", 2);
 		GameRegistry.register(this);
+		
+		ItemBlock itemBlock = new ItemBlock(this);
+		itemBlock.setRegistryName(name);
+		GameRegistry.register(itemBlock);
 	}
 	
+	/**
+	 * Gets the name of the ore. 
+	 * Useful if you only want the end part of the unlocalised name
+	 * @return
+	 */
 	public String getName()
 	{
 		return this.name;
