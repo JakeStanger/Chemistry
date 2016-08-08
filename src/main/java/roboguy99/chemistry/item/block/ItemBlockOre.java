9f84481c87a63305bf588cc09fbb80752a7ddbd0
Roboguy99 <Roboguy99@gmail.com>
2016-08-08 22:31:11 +0100

    Added getter for ore block

--- src/main/java/roboguy99/chemistry/item/block/ItemBlockOre.java
+++ src/main/java/roboguy99/chemistry/item/block/ItemBlockOre.java
@@ -2,11 +2,20 @@ package roboguy99.chemistry.item.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
+import roboguy99.chemistry.block.ore.BlockOre;
 
 public class ItemBlockOre extends ItemBlock
 {
-	public ItemBlockOre(Block block)
+	private BlockOre ore;
+	
+	public ItemBlockOre(BlockOre block)
 	{
 		super(block);
+		this.ore = block;
+	}
+	
+	public BlockOre getOre()
+	{
+		return this.ore;
 	}
 }
