a0173bad9d4800d8478a2d6344a625c083e0eb33
Roboguy99 <Roboguy99@gmail.com>
2016-08-10 01:54:02 +0100

    Added extra groups and changed group colours

--- src/main/java/roboguy99/chemistry/api/Group.java
+++ src/main/java/roboguy99/chemistry/api/Group.java
@@ -2,7 +2,7 @@ package roboguy99.chemistry.api;
 
 /**
  * Handles element groups and group colours.
- * TODO Add more groups and change elements accordingly
+ * TODO Change elements to new groups TODO Generate textures for new groups
  * @author Roboguy99
  *
  */
@@ -11,12 +11,16 @@ public enum Group
 	ALKALI_METALS("alkaliMetals"),
 	ALKALINE_EARTH_METALS("alkalineEarthMetals"),
 	TRANSITION_METALS("transitionMetals"),
-	OTHER_METALS("otherMetals"),
+	POST_TRANSITION_METALS("postTransitionMetals"),
+	LANTHANOIDS("lanthanoids"),
+	ACTINOIDS("actinoids"),
+	METALLOIDS("metalloids"),
 	NON_METALS("nonMetals"),
+	HALOGENS("halogens"),
 	NOBLE_GASES("nobleGases"),
 	SPECIAL("special");
 	
-	public static Group[] GROUPS = new Group[]{ALKALI_METALS, ALKALINE_EARTH_METALS, TRANSITION_METALS, OTHER_METALS, NON_METALS, NOBLE_GASES, SPECIAL};
+	public static Group[] GROUPS = new Group[]{ALKALI_METALS, ALKALINE_EARTH_METALS, TRANSITION_METALS, POST_TRANSITION_METALS, LANTHANOIDS, ACTINOIDS, METALLOIDS, NON_METALS, HALOGENS, NOBLE_GASES, SPECIAL};
 	
 	public final String groupName;
 	
@@ -38,22 +42,34 @@ public enum Group
 		switch(group)
 		{
 			case ALKALI_METALS:
-				groupColour = Colour.WHITE;
+				groupColour = Colour.ORANGE;
 				break;
 			case ALKALINE_EARTH_METALS:
-				groupColour = Colour.PINK;
+				groupColour = Colour.YELLOW;
 				break;
 			case TRANSITION_METALS:
 				groupColour = Colour.AQUA;
 				break;
-			case OTHER_METALS:
-				groupColour = Colour.ORANGE;
+			case POST_TRANSITION_METALS:
+				groupColour = Colour.DARK_AQUA;
+				break;
+			case LANTHANOIDS:
+				groupColour = Colour.DARK_GREEN;
+				break;
+			case ACTINOIDS:
+				groupColour = Colour.PINK;
+				break;
+			case METALLOIDS:
+				groupColour = Colour.DARK_BLUE;
 				break;
 			case NON_METALS:
 				groupColour = Colour.BRIGHT_GREEN;
 				break;
 			case NOBLE_GASES:
-				groupColour = Colour.YELLOW;
+				groupColour = Colour.DARK_AQUA;
+				break;
+			case HALOGENS:
+				groupColour = Colour.PURPLE;
 				break;
 			default:
 				groupColour = Colour.WHITE;
