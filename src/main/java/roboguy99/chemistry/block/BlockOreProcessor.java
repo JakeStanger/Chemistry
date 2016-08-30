b145f51cd541b421dc1749975ff432244e16abdb
Jake Stanger <jakestanger@gmail.com>
2016-08-30 17:38:45 +0100

    Open GUI instead of running code in block

--- src/main/java/roboguy99/chemistry/block/BlockOreProcessor.java
+++ src/main/java/roboguy99/chemistry/block/BlockOreProcessor.java
@@ -8,10 +8,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import roboguy99.chemistry.Chemistry;
 import roboguy99.chemistry.block.ore.BlockOre;
+import roboguy99.chemistry.handler.GuiHandler;
 import roboguy99.chemistry.item.block.ItemBlockOre;
 import roboguy99.chemistry.item.element.ItemElement;
 import roboguy99.chemistry.tile.TileOreProcessor;
@@ -30,7 +30,10 @@ public class BlockOreProcessor extends BlockTile
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) 
 	{
-		if(!world.isRemote && heldItem != null) 
+		if(!world.isRemote) player.openGui(Chemistry.INSTANCE, GuiHandler.GUI_BLOCK_ORE_PROCESSOR, world, pos.getX(), pos.getY(), pos.getZ());
+		return true;
+		
+		/*if(!world.isRemote && heldItem != null) TODO Move
 		{
 			HashMap<ItemElement, Integer> elements = assignRandomElements(heldItem.getItem()); //Get random elements based on ore
 			
@@ -40,11 +43,11 @@ public class BlockOreProcessor extends BlockTile
 				player.addChatMessage(new TextComponentString(element.getName() + " - " + elements.get(element))); 
 			}
 		}
-		return true;
+		return true;*/
 	}
 	
 	/**
-	 * Assigns random elements based on the item given, assuming the item is an ore.
+	 * Assigns random elements based on the item given, assuming the item is an ore. TODO Move
 	 * The quantity of elements is based on the ore's resource map.
 	 * @param item The item to assign elements to.
 	 * @return A map of assigned elements and quantities
