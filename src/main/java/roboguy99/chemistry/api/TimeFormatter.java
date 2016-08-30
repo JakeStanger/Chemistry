b7b7123a354a6251f061d5327f768a7a49e0c847
Jake Stanger <jakestanger@gmail.com>
2016-08-30 19:27:33 +0100

    Tidied commenting.

--- src/main/java/roboguy99/chemistry/api/TimeFormatter.java
+++ src/main/java/roboguy99/chemistry/api/TimeFormatter.java
@@ -1,15 +1,15 @@
 package roboguy99.chemistry.api;
 
-/**
- * Created by jake on 30/08/16.
- * Written by David Wallace
- * (http://codereview.stackexchange.com/questions/139970/convert-length-of-time-to-appropriate-unit/139972)
- */
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Formats a time, given in seconds, to the appropriate human-readable format.
+ * Written by David Wallace.
+ * <a href="http://codereview.stackexchange.com/questions/139970/convert-length-of-time-to-appropriate-unit/139972">See here</a>.
+ */
 public class TimeFormatter {
 	
 	private NavigableMap<Long,String> timeUnits = new TreeMap<>();
@@ -25,10 +25,10 @@ public class TimeFormatter {
 		timeUnits.put(TimeUnit.DAYS.toSeconds(365 * 1000000L * 1000), " billion years");
 	}
 	
-	public String format(long milliseconds) {
-		Map.Entry<Long,String> unitBelow = timeUnits.floorEntry(milliseconds);
+	public String format(long seconds) {
+		Map.Entry<Long,String> unitBelow = timeUnits.floorEntry(seconds);
 		
-		int time = (int) (milliseconds / unitBelow.getKey());
+		int time = (int) (seconds / unitBelow.getKey());
 		String formatted = time + unitBelow.getValue();
 		
 		if(time == 1 && unitBelow.getKey() < 1000000L) formatted = formatted.substring(0, formatted.length()-1); //Remove plural
