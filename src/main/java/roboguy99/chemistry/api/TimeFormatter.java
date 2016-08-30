3af50bac9d5e07ace1db4bdfeacc34e152036722
Jake Stanger <jakestanger@gmail.com>
2016-08-30 14:15:03 +0100

    Improved half-life system to use TreeMap and work in seconds.

---
+++ src/main/java/roboguy99/chemistry/api/TimeFormatter.java
@@ -0,0 +1,38 @@
+package roboguy99.chemistry.api;
+
+/**
+ * Created by jake on 30/08/16.
+ * Written by David Wallace
+ * (http://codereview.stackexchange.com/questions/139970/convert-length-of-time-to-appropriate-unit/139972)
+ */
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+
+public class TimeFormatter {
+	
+	private NavigableMap<Long,String> timeUnits = new TreeMap<>();
+	
+	public TimeFormatter() {
+		timeUnits.put(Long.MIN_VALUE, " is not a valid argument");
+		timeUnits.put(TimeUnit.SECONDS.toSeconds(1), " seconds");
+		timeUnits.put(TimeUnit.MINUTES.toSeconds(1), " minutes");
+		timeUnits.put(TimeUnit.HOURS.toSeconds(1), " hours");
+		timeUnits.put(TimeUnit.DAYS.toSeconds(1), " days");
+		timeUnits.put(TimeUnit.DAYS.toSeconds(365), " years");
+		timeUnits.put(TimeUnit.DAYS.toSeconds(365 * 1000000L), " million years");
+		timeUnits.put(TimeUnit.DAYS.toSeconds(365 * 1000000L * 1000), " billion years");
+	}
+	
+	public String format(long milliseconds) {
+		Map.Entry<Long,String> unitBelow = timeUnits.floorEntry(milliseconds);
+		
+		int time = (int) (milliseconds / unitBelow.getKey());
+		String formatted = time + unitBelow.getValue();
+		
+		if(time == 1) formatted = formatted.substring(0, formatted.length()-1); //Remove plural
+		
+		return formatted;
+	}
+}
--- src/main/java/roboguy99/chemistry/item/element/elements/special/ElementRadioactive.java
+++ src/main/java/roboguy99/chemistry/item/element/elements/special/ElementRadioactive.java
@@ -8,6 +8,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import org.lwjgl.input.Keyboard;
 import roboguy99.chemistry.api.Colour;
+import roboguy99.chemistry.api.TimeFormatter;
 import roboguy99.chemistry.item.element.ItemElement;
 
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.List;
 /**
  * A radioactive element.
  * Unlike the base element, this includes a half-life system.
- * The half-life is in Minecraft minutes, and once it runs out the parent itemstack halves in size.
+ * The half-life is in Minecraft minutes, and once it runs out the parent ItemStack halves in size.
  * This is rounded down, so a stack of 3 will decay to a stack of 1, and a stack of 1 will be deleted.
  * One Minecraft minute is equal to 1/20 of a real minute (3 real seconds).
  * @author Jake Stanger
@@ -31,7 +32,7 @@ public abstract class ElementRadioactive extends ItemElement
 	/**
 	 * The time between decay updates in milliseconds
 	 */
-	private static final int UPDATE_GAP = 3000;
+	private static final int UPDATE_GAP = 1000;
 	
 	
 	public ElementRadioactive()
@@ -39,14 +40,6 @@ public abstract class ElementRadioactive extends ItemElement
 		this.HALF_LIFE = this.giveHalfLife();
 	}	
 	
-	/**
-	 * Get the half-life of the element.
-	 * @return The half-life of the element, in minutes.
-	 */
-	public long getHALF_LIFE()
-	{
-		return this.HALF_LIFE;
-	}
 	
 	/**
 	 * The half-life of the element, in minutes.
@@ -56,11 +49,11 @@ public abstract class ElementRadioactive extends ItemElement
 	/**
 	 * Runs when the item ticks.
 	 * Updates half-life.
-	 * @param stack
-	 * @param world
-	 * @param entity
-	 * @param par4
-	 * @param par5
+	 * @param stack The ItemStack
+	 * @param world The World
+	 * @param entity The player
+	 * @param par4 ?
+	 * @param par5 ?
 	 */
 	@Override
 	public void onUpdate(ItemStack stack, World world, Entity entity, int par4, boolean par5)
@@ -123,59 +116,15 @@ public abstract class ElementRadioactive extends ItemElement
 		super.addInformation(stack, player, tooltip, advanced);
 		if(Keyboard.isKeyDown(Minecraft.getMinecraft().gameSettings.keyBindSneak.getKeyCode()))
 		{
-			try
-			{
-				tooltip.add(Colour.BRIGHT_GREEN + "Radioactive: " + this.getDecayTimeReadable(stack.getTagCompound().getLong("timeUntilDecay")) + " until decay");
-			}
-			catch(NullPointerException e)
-			{
-				tooltip.add(Colour.BRIGHT_GREEN + "Radioactive: " + this.getDecayTimeReadable(this.HALF_LIFE) + " until decay");
-			}
-		}
-	}
-	
-	/**
-	 * Scale the units of the given decay time into a more suitable unit.
-	 * For example 60 minutes is converted to 1 hour.
-	 * This will scale up to billions of years.
-	 * @param decayTime The time until the element decays
-	 * @return The time until the element decays, in a more readable format
-	 */
-	private String getDecayTimeReadable(long decayTime)
-	{
-		String readable = decayTime + " minutes";
-		if(decayTime > 60)
-		{
-			decayTime /= 60;
-			readable = decayTime + " hours";
+			TimeFormatter timeFormatter = new TimeFormatter();
 			
-			if(decayTime > 24)
-			{
-				decayTime /= 24;
-				readable = decayTime + " days";
-				
-				if(decayTime > 365)
-				{
-					decayTime /=365;
-					readable = decayTime + " years";
-					
-					if(decayTime > 1000000)
-					{
-						decayTime /= 1000000;
-						readable = decayTime + "mn years";
-						
-						if(decayTime > 1000)
-						{
-							decayTime /= 1000;
-							readable = decayTime + "bn years"; 
-						}
-					}
-				}
-			}
+			long time;
+			
+			NBTTagCompound compound = stack.getTagCompound();
+			if(compound != null && compound.hasKey("timeUntilDecay")) time = compound.getLong("timeUntilDecay");
+			else time = this.HALF_LIFE;
+			
+			tooltip.add(Colour.BRIGHT_GREEN + "Radioactive: " + timeFormatter.format(time) + " until decay");
 		}
-		
-		if(decayTime == 1) readable = readable.substring(0, readable.length()-1); //Remove plural
-		
-		return readable;
 	}
 }
